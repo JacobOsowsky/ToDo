f0b175dd7a65c16d536cbe24c380065af333d9de
Jacob <j.osowski15@interia.pl>
2021-03-17 21:46:42 +0100

    create structure of adding component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {  }
+    state = { 
+        text: "",
+        checked: false,
+        date: "2021-04-01"
+     }
     render() { 
         return ( 
             <div className="add">
-                Dodaj Task
+                <input type="text" placeholder="wpisz zadanie" id="text" value={this.state.text}></input>
+                <input type="checkbox" id="checked" checked={this.state.checked}></input>
+                <label htmlFor="checked">Priorytet</label> <br/>
+                <label htmlFor="date">Do kiedy</label>
+                <input type="date" id="date" value={this.state.date} min="2021-03-17" max="2021-05-01"></input> <br/>
+                <button>Dodaj</button>
+                
             </div>
          );
     }
